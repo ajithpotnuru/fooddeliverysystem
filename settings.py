4cd9ce440ea7eeed3e5dee9f220ef64022a6b577
Ajith potnuru <151811368+ajithpotnuru@users.noreply.github.com>
2026-01-02 17:49:36 +0530

    settings.py

---
+++ settings.py
@@ -0,0 +1,18 @@
+AUTH_USER_MODEL = 'your_app.User'
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'your_app',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
